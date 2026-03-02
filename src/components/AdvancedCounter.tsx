e45e0bcb19e324d4b6b903697ba5de4e9de068e9
Phillip Thapa <1thapat@gmail.com>
2026-03-02 12:55:26 -0500

    setup state for all required variables

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 
+const COUNT_KEY = "count_counter"
+
 export default function AdvancedCounter(){
 
 
-    //default value of the count is zero
-    const [count, setCount] = useState<number>(0);
+    //checks if Count_key local storage key value is emply if so sets its value to zero
+    const [count, setCount] = useState<number>(() =>  localStorage.getItem(COUNT_KEY) !==null ? Number(localStorage.getItem(COUNT_KEY)):0);
+
+    //history state
+    const [countHistory, setCountHistory] = useState<number[]>(()=>[count]);
+
+
+    //seting up state for step
+    const [step, setStep] = useState<number>(1);
 
 
 
@@ -19,15 +28,15 @@ export default function AdvancedCounter(){
     }
 
     return (
-        <div>
-            <div>
-                <h2>Counter</h2>
+        <div className="w-full max-w-lg rounded-xl border bg-white p-6 shadow">
+            <div >
+                <h2 className="text-center text-sm text-gray-600">Counter</h2>
                 <h1>Current Count: </h1>
             </div>
-            <div >
-                <button>Increment</button>
-                <button>Decrement</button>
-                <button>Reset</button>
+            <div className="mt-6 flex items-center justify-center gap-6">
+                <button className="basis-64">Increment</button>
+                <button className="basis-64">Decrement</button>
+                <button className="basis-64">Reset</button>
             </div>
         </div>
     )
